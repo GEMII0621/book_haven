97f496ca37563125f385739ba3ada31957401b9a
George Moss, II <GeorgeMoss0621@Gmail.com>
2025-02-27 17:30:30 -0500

    Update

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+document.querySelector(".visit-btn").addeventListener("click", function () {
+    alert(Book Haven | 123 Bookstore Rd | Bookstore, SC 12345);
+                    });
